0f7d0ad84abed6e51d53fa79323ef6ad1f9cff7d
kyle <jg.kang@fitpet.co.kr>
2023-05-08 17:56:39 +0900

    [Next] map함수로 반복자료 표현하기

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,13 +1,18 @@
 export default function List() {
+  let products: String[] = ["Tomatoes", "Pasta", "Coconut"];
+
+  let productCards = products.map((product, i) => {
+    return (
+      <div className="food" key={i}>
+        <h4>{product} $40</h4>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {productCards}
     </div>
   );
 }
