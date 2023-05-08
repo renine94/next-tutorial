e16458500bd0388c7c523062360199147965701d
kyle <jg.kang@fitpet.co.kr>
2023-05-08 18:05:27 +0900

    [Next] 이미지 최적화 컴포넌트 사용하기 Image

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function List() {
   let products: String[] = ["Tomatoes", "Pasta", "Coconut"];
 
   let productCards = products.map((product, i) => {
     return (
       <div className="food" key={i}>
+        <Image
+          src={`/food${i}.png`}
+          alt="help text"
+          className="food-img"
+          width={100}
+          height={100}
+        />
         <h4>{product} $40</h4>
       </div>
     );
